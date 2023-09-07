8eae1b5ac4d43d5aefcee0ca7eb57f12b8c57218
Felipe Cupitó <fcupito@itba.edu.ar>
2023-09-07 20:38:35 -0300

    make thead safe all ParkRepository func

--- server/src/main/java/ar/edu/itba/pod/server/models/AttractionDay.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/AttractionDay.java
@@ -4,35 +4,41 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class AttractionDay {
     private final Integer soltNumber;
-    private boolean hasCapacity; //solo se debe cambiar en setCapacity
+    private boolean capacityFlag; //solo se debe cambiar en setCapacity
     private final List<AttractionSlot> slots = new ArrayList<>();
+
+    private final ReadWriteLock capacityLock = new ReentrantReadWriteLock(true);  //por orden de llegada
 
     public AttractionDay(Integer soltNumber) {
         this.soltNumber = soltNumber;
-        this.hasCapacity = false;
-
+        this.capacityFlag = false;
 
         for (int i = 0; i < soltNumber; i++) {
             slots.add(new AttractionSlot());
         }
     }
 
-    public void setCapacity(Integer capacity){
-        //TODO: debe ser thead safe y bloquear el acceso a la lista de slots
+    private void setCapacityFlag(){
+        if (capacityFlag){
+            throw new IllegalArgumentException("Attraction capacity already set");
+        }
+        capacityFlag = true;
+    }
+
+    synchronized public void setCapacity(Integer capacity){
+        //TODO: debe ser thead safe y bloquear el acceso a la lista de slots para las add reservas
         if (capacity < 0) {
             throw new IllegalArgumentException("Attraction capacity must be greater than 0");
         }
 
-        if (hasCapacity){
-            throw new IllegalArgumentException("Attraction capacity already set");
-        }
-        hasCapacity = true;
+        setCapacityFlag();
 
         Queue<Reservation> to_reassign = new ArrayDeque<>();
-
         //confirmo las reservas que pueda y agrego las que no pueda a la cola de reasignar
         for (int i = 0; i < soltNumber; i++) {
             //seteo la capacidad de cada slot y agrego las reservas que se puedan
@@ -43,7 +49,10 @@ public class AttractionDay {
         while(!to_reassign.isEmpty()){
             to_reassign.poll().cancel();
         }
+    }
 
+    synchronized public void addReservation(Reservation reservation){
+        //TODO: debe estar sinconizado con el setCapacity
     }
 
 }
--- server/src/main/java/ar/edu/itba/pod/server/models/ParkRepository.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/ParkRepository.java
@@ -3,39 +3,52 @@ package ar.edu.itba.pod.server.models;
 import services.Park;
 
 import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ParkRepository {
     private final Map<String, Attraction> attractions = new HashMap<>();
     private final Map<UUID, Pass> passes = new HashMap<>();
+    private final ReadWriteLock attractionLock = new ReentrantReadWriteLock(true);  //por orden de llegada
+    private final ReadWriteLock passLock = new ReentrantReadWriteLock(true);  //por orden de llegada
 
     ////////////////////
     //  Admin Service //
-    ///////////////////
+    ////////////////////
     /**
      * Adds a new attraction to the park
      */
     public void addAttraction(String name, int openTime, int closeTime, int minutesPerSlot) {
-        Attraction newAttraction = new Attraction(name, openTime, closeTime, minutesPerSlot);
+        attractionLock.writeLock().lock();
+        try{
+            Attraction newAttraction = new Attraction(name, openTime, closeTime, minutesPerSlot);
+            //Check if attraction already exists
+            if(attractions.containsKey(name)) {
+                throw new IllegalArgumentException("Attraction already exists");
+            }
+            attractions.put(name, newAttraction);
 
-        //Check if attraction already exists
-        if(attractions.containsKey(name)) {
-            throw new IllegalArgumentException("Attraction already exists");
+        } finally {
+            attractionLock.writeLock().unlock();
         }
-
-        attractions.put(name, newAttraction);
     }
 
     /**
      * Adds a new pass to the park
      */
     public void addPass(String uuidString, Park.PassType type, int day) {
-        Pass newPass = new Pass(uuidString, type, day);
+        passLock.writeLock().lock();
+        try{
+            Pass newPass = new Pass(uuidString, type, day);
 
-        if (passes.containsKey(newPass.getUuid())) {
-            throw new IllegalArgumentException("Pass already exists");
+            if (passes.containsKey(newPass.getUuid())) {
+                throw new IllegalArgumentException("Pass already exists");
+            }
+
+            passes.put(newPass.getUuid(), newPass);
+        }finally {
+            passLock.writeLock().unlock();
         }
-
-        passes.put(newPass.getUuid(), newPass);
     }
 
     /**
