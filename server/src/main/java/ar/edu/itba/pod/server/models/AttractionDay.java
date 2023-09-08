f47e5d94adfb03f38a13da9a4eb8aa91ab6c038e
Felipe Cupitó <fcupito@itba.edu.ar>
2023-09-08 20:00:25 -0300

    make thread safe AttractionDay

--- server/src/main/java/ar/edu/itba/pod/server/models/AttractionDay.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/AttractionDay.java
@@ -6,13 +6,12 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class AttractionDay {
     private final Integer soltNumber;
-    private boolean capacityFlag; //solo se debe cambiar en setCapacity
+    private boolean capacityFlag = false; //solo se debe cambiar en setCapacity
     private final List<AttractionSlot> slots = new ArrayList<>();
     private final ReadWriteLock capacityLock = new ReentrantReadWriteLock(true);  //por orden de llegada
 
     public AttractionDay(Integer soltNumber) {
         this.soltNumber = soltNumber;
-        this.capacityFlag = false;
 
         for (int i = 0; i < soltNumber; i++) {
             slots.add(new AttractionSlot());
@@ -27,52 +26,73 @@ public class AttractionDay {
     }
 
     synchronized public void setCapacity(Integer capacity){
-        //TODO: debe ser thead safe y bloquear el acceso a la lista de slots para las add reservas
         Utils.checkCapacity(capacity);
-
-        setCapacityFlag();
-
-        Queue<Reservation> to_reassign = new ArrayDeque<>();
-        //confirmo las reservas que pueda y agrego las que no pueda a la cola de reasignar
-        for (int i = 0; i < soltNumber; i++) {
-            //seteo la capacidad de cada slot y agrego las reservas que se puedan
-            to_reassign = slots.get(i).setCapacity(capacity, to_reassign);
-        }
-
-        //si todavía tengo reservas por reasignar, las cancelo
-        while(!to_reassign.isEmpty()){
-            to_reassign.poll().cancelReservation();
-        }
-    }
-
-    synchronized public Status addReservation(Pass pass, Integer slotIndex){
-        checkIndex(slotIndex);
-
-        //se bloquea el acceso a la lista de slots cuando se esta setieando la capacidad de este dia
+        //se bloquea el acceso a la lista de slots para que no se agregue una nueva
+        // reserva mientras se esta setieando la capacidad de este dia
         capacityLock.writeLock().lock();
         try{
-            return slots.get(slotIndex).addReservation(pass);
+            setCapacityFlag();
+            Queue<Reservation> to_reassign = new ArrayDeque<>();
+
+            //confirmo las reservas que pueda y agrego las que no pueda a la cola de reasignar
+            for (int i = 0; i < soltNumber; i++) {
+                //seteo la capacidad de cada slot y agrego las reservas que se puedan
+                to_reassign = slots.get(i).setCapacity(capacity, to_reassign);
+            }
+
+            //si todavía tengo reservas por reasignar, las cancelo
+            while(!to_reassign.isEmpty()){
+                to_reassign.poll().cancelReservation();
+            }
 
         }finally {
             capacityLock.writeLock().unlock();
         }
     }
 
-    public SlotAvailabilityResponse getSingleSlotAvailability(Integer slotIndex) {
-        checkIndex(slotIndex);
+    synchronized public Status addReservation(Pass pass, Integer slotIndex){
+        //se bloquea el acceso a la lista de slots cuando se esta setieando la capacidad de este dia
+        capacityLock.readLock().lock();
+        try{
+            checkIndex(slotIndex);
+            return slots.get(slotIndex).addReservation(pass);
 
-        return slots.get(slotIndex).getSingleSlotAvailability();
+        }finally {
+            capacityLock.readLock().lock();
+        }
+    }
+
+    public SlotAvailabilityResponse getSingleSlotAvailability(Integer slotIndex) {
+        //se bloquea el acceso a la lista de slots cuando se esta setieando la capacidad de este dia
+        capacityLock.readLock().lock();
+        try {
+            checkIndex(slotIndex);
+            return slots.get(slotIndex).getSingleSlotAvailability();
+        }finally {
+            capacityLock.readLock().unlock();
+        }
     }
 
     public void confirmReservation(Pass pass, int slotIndex) {
-        checkIndex(slotIndex);
-        slots.get(slotIndex).confirmReservation(pass);
-
+        //se bloquea el acceso a la lista de slots cuando se esta setieando la capacidad de este dia
+        capacityLock.readLock().lock();
+        try{
+            checkIndex(slotIndex);
+            slots.get(slotIndex).confirmReservation(pass);
+        }finally {
+            capacityLock.readLock().unlock();
+        }
     }
 
     public void cancelReservation(Pass pass, int slotIndex) {
-        checkIndex(slotIndex);
-        slots.get(slotIndex).cancelReservation(pass);
+        //se bloquea el acceso a la lista de slots cuando se esta setieando la capacidad de este dia
+        capacityLock.readLock().lock();
+        try{
+            checkIndex(slotIndex);
+            slots.get(slotIndex).cancelReservation(pass);
+        }finally {
+            capacityLock.readLock().unlock();
+        }
     }
 
     private void checkIndex(int index){
@@ -80,5 +100,4 @@ public class AttractionDay {
             throw new IllegalArgumentException("Invalid slot index");
         }
     }
-
 }
