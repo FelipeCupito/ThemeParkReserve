1086d948bb777beb7f4a31f7ab562146b429a181
Alan Sartorio <alan42ga@hotmail.com>
2023-09-08 12:50:25 -0300

    Add outPath property to Query client

--- client/src/main/java/ar/edu/itba/pod/client/query/actions/QueryClientAction.java
+++ client/src/main/java/ar/edu/itba/pod/client/query/actions/QueryClientAction.java
@@ -4,16 +4,20 @@ import ar.edu.itba.pod.client.ClientAction;
 import ar.edu.itba.pod.client.properties.PropertyManager;
 import ar.edu.itba.pod.client.properties.exceptions.PropertyException;
 
+import java.nio.file.Path;
+
 abstract class QueryClientAction implements ClientAction {
     int day;
+    Path outPath;
 
     public QueryClientAction(PropertyManager properties) throws PropertyException {
         // TODO: Check argument naming
         day = properties.getDayOfYearProperty("day");
+        outPath = Path.of(properties.getProperty("outPath"));
     }
 
     @Override
     public String toString() {
-        return String.format("{ day: %d }", day);
+        return String.format("{ day: %d, outPath: \"%s\" }", day, outPath);
     }
 }
