bdfc4309d092b1a95121f8800a711fcbc06c0411
Alan Sartorio <alan42ga@hotmail.com>
2023-09-11 01:52:21 -0300

    Implement ConfirmAction and CancelAction

--- client/src/main/java/ar/edu/itba/pod/client/book/actions/CancelAction.java
+++ client/src/main/java/ar/edu/itba/pod/client/book/actions/CancelAction.java
@@ -3,8 +3,11 @@ package ar.edu.itba.pod.client.book.actions;
 import ar.edu.itba.pod.client.Clients;
 import ar.edu.itba.pod.client.properties.PropertyManager;
 import ar.edu.itba.pod.client.properties.exceptions.PropertyException;
+import ar.edu.itba.pod.client.serializers.DayOfYearSerializer;
+import ar.edu.itba.pod.client.serializers.TimeSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import services.Park;
 
 public class CancelAction extends ReservationAction {
     private static final Logger logger = LoggerFactory.getLogger(CancelAction.class);
@@ -15,7 +18,23 @@ public class CancelAction extends ReservationAction {
 
     @Override
     public void run(Clients clients) {
-        // TODO: Implement
+        clients.getReservationService().cancelReservation(
+                Park.ReservationInfo.newBuilder()
+                        .setUserId(visitor)
+                        .setDay(day)
+                        .setAttractionName(attraction)
+                        .setOpenTime(slot)
+                        .build()
+        );
+
+        // TODO: Output format not specified in spec?
+        System.out.printf(
+                "The reservation for %s at %s on the day %s was %s\n",
+                attraction,
+                new TimeSerializer().serialize(slot),
+                new DayOfYearSerializer().serialize(day),
+                "CANCELLED"
+        );
     }
 
     @Override
